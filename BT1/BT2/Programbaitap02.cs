d7d31c3b4b8a5774f494495b8222de5952723a58
ltu27 <ltu27@TLUCD>
2023-12-29 13:55:40 +0700

    Add project files.

---
+++ BT1/BT2/Programbaitap02.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    class Account
+    {
+        private string id;
+        private string name;
+        private int balance;
+        public Account(string id, string name, int balance)
+        {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+        public string getId()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void credit(int amount)
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+        public void debit(int amount)
+        {
+            if (amount <= balance)
+                balance -= amount;
+            else
+                Console.WriteLine("So du khong du de thuc hien giao dich!");
+        }
+        public void tranferTo(Account acc, int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                acc.balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Giao dich thuc hien khong thanh cong");
+            }
+        }
+        class ProgramBaiTap2
+        {
+            static void Main(string[] args)
+            {
+                Account my = new Account("2321160019", "Tu Luc", 1200);
+
+                Console.Write("Inter id value:");
+                string id = Console.ReadLine();
+
+                Console.Write("Inter name value:");
+                string name = Console.ReadLine();
+
+                Console.Write("Inter balance value:");
+                int balance = int.Parse(Console.ReadLine());
+
+                Account myFriend = new Account(id, name, balance);
+
+                Console.WriteLine("-----Thong tin hai tai khoan ban dau----");
+                Console.WriteLine("My account: [id:{0}, name:{1}, balance:{2}] ",
+                    my.getId(), my.getName(), my.getBalance());
+                Console.WriteLine("My friend account: [id:{0}, name:{1}, balance:{2}]",
+                    myFriend.getId(), myFriend.getName(), myFriend.getBalance());
+
+                //rut tien
+                my.debit(500);
+                my.tranferTo(myFriend, 340);
+
+                //ket qua sau khi thuc hien mot so hanh vi( nap, tur, chuyen tien)
+                Console.WriteLine("-----Thong tin hai tai khoan sau khi thuc hien giao dich----");
+                Console.WriteLine("My account:[id:{0}, name:{1}, balance:{2}]", my.getId(),
+                    my.getName(), my.getBalance());
+                Console.WriteLine("My friend account: [id:{0}, name:{1}, balance:{2}]",
+                    myFriend.getId(), myFriend.getName(), myFriend.getBalance());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BT1/LaptrinhOOP/Programbaitap01.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptrinhOOP
+{
+    class rectangle
+    {
+        private double length;
+        private double width;
+        public rectangle()
+        {
+            length = 1.0;
+            width = 1.0;
+        }
+        public rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public void setLength(double length)
+        {
+            this.length = length;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public void setWidth(double width)
+        {
+            this.width = width;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public double findArea()
+        {
+            return length * width;
+        }
+        public double findPermeter()
+        {
+            return (length + width) * 2;
+        }
+    }
+        class Programbaitap01
+        {
+            static void Main(string[] args)
+            {
+            rectangle r1; //Khai báo r1
+            rectangle r2; //Khai báo r2
+            r1 = new rectangle(); //Khởi tạo r1
+            Console.Write(" Cho biet chieu dai:");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write(" Cho biet chieu rong:");
+            double width = double.Parse(Console.ReadLine()); ;
+            r2 = new rectangle(length, width); //Khoi tao r2
+            Console.WriteLine("------Thong tin hinh chu nhat r1-------");
+            Console.WriteLine(" Chieu dai: {0}\nChieu rong: {1}\nDien tich: {3}",
+                r1.getLength(),r1.getWidth(),r1.findArea(),r1.findPermeter());
+            Console.WriteLine("------Thong tin hinh chu nhat r2-------");
+            Console.WriteLine(" Chieu dai: {0}\nChieu rong: {1}\nDien tich: {3}",
+                r2.getLength(), r2.getWidth(), r2.findArea(), r2.findPermeter());
+            Console.ReadLine();
+        }
+        }
+    }
+
